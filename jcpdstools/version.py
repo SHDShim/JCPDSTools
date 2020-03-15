88789591d7e36cc8a1e983cafd85999a0d9a8a4e
Sang-Heon Dan Shim <SHDShim@gmail.com>
2020-03-14 18:57:50 -0700

    0.0.7 Block dioptas JCPDS output because the output files crash PeakPo.

--- jcpdstools/version.py
+++ jcpdstools/version.py
@@ -4,8 +4,9 @@ JCPDSTools version
 Todo:
 
 """
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 """
+0.0.7 Block dioptas JCPDS output function due to crash of peakpo.
 0.0.6 Add Dioptas JCPDS output.  However, it is less tested than other functions.
 0.0.5 Default comment writings and filenames change
 0.0.4 Clean up UI.
